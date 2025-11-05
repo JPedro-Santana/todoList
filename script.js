3d97b9a42a1dc5993921b745db4eaf48f0403835
Jpedro <joao.pedrosousa5@gmail.com>
2025-11-05 12:38:11 -0300

    Updates

--- script.js
+++ script.js
@@ -1,27 +1,78 @@
 document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
-    const addButton = document.getElementById('add-button');
+    const addButton = document.getElementById('add-btn');
     const taskList = document.getElementById('task-list');
+    const todoContainer = document.querySelector('.todo-container');
+    const progressBar = document.getElementById('progress');
+    const progressNumbers = document.getElementById('numbers');
 
-    const addTask = (event) => {
-        event.preventDefault();
-        const taskText = taskInput.value.trim();
+    const toggleEmpty = () => {
+        lenght === 0 ? 'block' : 'none';
+        todoContainer.style.width = taskList.children.length > 0 ? '100%' : '50%';
+    };
+
+    const updateProgress = (checkCompletion = true) => {
+        const totalTasks = taskList.children.length;
+        const completedTasks = taskList.querySelectorAll('.checkbox:checked').length
+        const motivationalText = document.getElementsByClassName('motivation');
+    };
+
+    const addTask = (text, completed = false) => {
+        const taskText = text || taskInput.value.trim();
         if(!taskText){
             return;
         }
 
         const li = document.createElement('li');
-        li.innerHTML = `<input type="checkbox" class="checkbox>
-        <span>${taskText}</span>" `
-        li.textContent = taskText;
+        li.innerHTML = `<input type="checkbox" class="checkbox" ${completed ? 'checked' : ''}/>
+        <span>${taskText}</span> 
+        <div class="task-buttons">
+        <button class="edit-btn"><i class="fa-solid fa-pen"></i></button>
+        <button class="delete-btn"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        `;
+        
+        const checkbox = li.querySelector('.checkbox')
+        const editBtn = li.querySelector('.edit-btn')
+        
+        if(completed){
+            li.classList.add('completed');
+            editBtn.disabled = true;
+            editBtn.style.opacity = '0.5';
+            editBtn.style.pointerEvents = 'none';
+        }
+
+        checkbox.addEventListener('change', () =>{
+            const isChecked = checkbox.checked;
+            li.classList.toggle('completed', isChecked);
+            editBtn.disabled = isChecked;
+            editBtn.style.opacity = isChecked ? '0.5' : '1';
+            editBtn.style.pointerEvents = isChecked ? 'none' : auto;
+        });
+        
+        editBtn.addEventListener('click', () => {
+            if(!checkbox.checked){
+                taskInput.value = li.querySelector('span').textContent;
+                li.remove();
+                toggleEmpty();
+            }
+        });
+
+        li.querySelector('.delete-btn').addEventListener('click', () => {
+            li.remove();
+            toggleEmpty();
+        });
+
         taskList.appendChild(li);
         taskInput.value = '';
+        toggleEmpty();
     };
 
-    addButton.addEventListener('click', addTask);
+    addButton.addEventListener('click', () => addTask());
     taskInput.addEventListener('keypress', (e) => {
         if(e.key === 'Enter'){
-            addTask(e);
+            e.preventDefault();
+                addTask();
         }
     });
 });
